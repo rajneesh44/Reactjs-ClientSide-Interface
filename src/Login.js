511d98df3915f1ffe181d0adccbfbd9332e6cf93
rajneesh44 <rajneeshsharma338@gmail.com>
2020-01-31 23:49:52 +0530

    firebase:login added

--- src/Login.js
+++ src/Login.js
@@ -7,6 +7,7 @@ class Login extends Component {
         super(props);
         this.login=this.login.bind(this);
         this.handleChange=this.handleChange.bind(this);
+        this.signup = this.signup.bind(this);
         this.state={
             email:'',
             password:''
@@ -20,6 +21,13 @@ class Login extends Component {
             console.log(error);
         });
     }
+    signup(e){
+        e.preventDefault();
+        fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).catch((error) => {
+            console.log(error);
+        });
+    }
+
 
     handleChange(e) {
         this.setState({[e.target.name]: e.target.value});
