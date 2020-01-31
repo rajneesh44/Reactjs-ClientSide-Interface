9c3a072fee9b50803a1fb03961d8dd4ff8e3b215
rajneesh44 <rajneeshsharma338@gmail.com>
2020-01-31 22:34:23 +0530

    authListener added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import fire from './config/fire'
 
 function App() {
+
+  constructor(props){
+    super(props);
+    this.state= {
+      user: {},
+    }
+  }
+
+  componentDidMount(){
+    this.authListener();
+  }
+
+  authListener(){
+    fire.auth().onAuthStateChanged((user) => {
+     // console.log(user);
+      if(user){
+        this.setState({user});
+     //   localStorage.setItem('user', user.uid);
+      }
+      else{
+        this.setState({user:null});
+     //   localStorage.removeItem('user');
+      }
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+     {this.state.user ? (<Home />) : (<Login/>)}
+
+    </div>
+  );
+}
+
+export default App;
+
+
+//part under main divs
+
+ {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload and yes do one thing.
@@ -18,9 +56,4 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+      </header> */}
