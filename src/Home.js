bd7ea6ef3a3dea6001eb7392978923ddb928a573
rajneesh44 <rajneeshsharma338@gmail.com>
2020-01-31 23:49:21 +0530

    added Login Feature

--- src/Home.js
+++ src/Home.js
@@ -14,7 +14,8 @@ class Home extends Component {
     render() {
         return (
             <div className="col-md-6">
-                <h1>Ypu are home</h1>
+                <h1>You are home</h1>
+                <button onClick={this.logout}>Logout</button>
             </div>
             );
     }
