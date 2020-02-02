a56b94600b7863714822e026d95f8130615b48d6
rajneesh44 <rajneeshsharma338@gmail.com>
2020-02-03 01:54:14 +0530

    Feature Added: Google SignIn

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { Link, BrowserRouter, MemoryRouter, Router} from 'react-router-dom';
+import { Link, BrowserRouter} from 'react-router-dom';
 import fire from './config/fire';
 import Signup from './Signup';
+import zh from './assets/zh.jpg';
 
-
+const GOOGLE_BUTTON_ID = 'google-sign-in-button';
 class Login extends Component {
     constructor(props){
         super(props);
@@ -36,154 +37,54 @@ class Login extends Component {
         this.setState({[e.target.name]: e.target.value});
     }
 
-
-      handleFormReset = () => {
-        this.setState({ email: ' ', password:' ' })
-      }
-
-      handleButtonClick = () => {
-        this.form.reset() // resets "username" field to "admin"
-      }
+    componentDidMount() {
+          window.gapi.signin2.render(
+            GOOGLE_BUTTON_ID,
+            {
+              width: 130,
+              height: 50,
+            //   onsuccess: this.login,
+              theme:'dark',
+            },
+          );
+        }
+      
+      
 
     render(){
         return(
-            <div>
+
+           <div> 
+               <div class = "pic-div"> 
+               <img src={zh} class="pic" alt="zhcet-logo" />   
+                </div>
                 <form>
                     <div class= "form-group">
                         <h1>AMS-ZHCET</h1>
                         {/* <label for="exampleInputEmail"> Email address</label> */}
                         <input value ={this.state.email} onChange={this.handleChange} type="email" name="email"
                         class="form-control" id="exampleInputEmail" aria-describedby="emailHelp"
-                        placeholder="Enter email" value={this.state.email}/>
+                        placeholder="Enter email" value={this.state.email} required/>
                         
                     </div>
 
                     <div class= "form-group">
                         {/* <label for="exampleInputPassword"> Password</label> */}
                         <input value ={this.state.password} onChange={this.handleChange} type="password" name="password"
-                        class="form-control" id="exampleInputPassword" placeholder="Password" value={this.state.password}/>
+                        class="form-control" id="exampleInputPassword" placeholder="Password" value={this.state.password} required/>
                       
                     </div>
                     <button type = "submit" onClick={this.login} class= "primary">Login</button>
-                    
                     <button onClick={this.signup} class="success">SignUp</button>  
-                    {/* <button onClick={this.handleButtonClick}>Reset</button>                                           */}
-                    {/* <Link to={'/Signup'}>
-                        <button > Click Me </button>
-                    </Link>
-                   
-                 
                     
-
-                    {/* <BrowserRouter>
-                        <button><Link to='/Signup' target='./Signup'> Click Me  </Link></button>
-                    </BrowserRouter> */}
-                       <BrowserRouter>
-                       <Link to="/Signup.js">signup</Link>
-                     </BrowserRouter>
+                    {/* adding google sign in button */}
+                    <div id={GOOGLE_BUTTON_ID} data-onsuccess= {this.login} />
                     
                 </form>
+                <h3>Developed by: <a href="https://github.com/rajneesh44">Rajneesh Sharma</a> | <a href="https://github.com/Bhanups123">Bhanu Pratap Singh</a></h3>
             </div>
         );
     }
 }
 
 export default Login;
-
-
-
-
-// import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
-// import fire from './config/fire';
-
-// class Login extends Component {
-// 	constructor(props) {
-// 		super(props);
-//         this.login=this.login.bind(this);
-//         this.handleChange=this.handleChange.bind(this);
-// 		this.state = {
-// 			email: '',
-// 			password: ''
-// 		};
-
-// 		this.update = this.update.bind(this);
-
-// 		this.displayLogin = this.displayLogin.bind(this);
-// 	}
-
-    
-//     login(e){
-//         e.preventDefault();
-//         fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u) =>{
-//         }).catch((error) => {
-//             console.log(error);
-//         });
-//     }
-//     signup(e){
-//         e.preventDefault();
-//         fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).catch((error) => {
-//             console.log(error);
-//         });
-//     }
-
-
-//     handleChange(e) {
-//         this.setState({[e.target.name]: e.target.value});
-//     }
-
-
-// 	update(e) {
-// 		let name = e.target.name;
-// 		let value = e.target.value;
-// 		this.setState({
-// 			[name]: value
-// 		});
-// 	}
-
-// 	displayLogin(e) {
-// 		e.preventDefault();
-// 		console.log('You are logged in');
-// 		console.log(this.state);
-// 		this.setState({
-// 			email: '',
-// 			password: ''
-// 		});
-// 	}
-
-// 	render() {
-// 		return (
-// 			<div className="login">
-// 				<form onSubmit={this.displayLogin}>
-// 					<h2>Login</h2>
-// 					<div className="username">
-// 						<input
-// 							type="text"
-// 							placeholder="Username..."
-// 							value={this.state.email}
-// 							onChange={this.update}
-// 							name="email"
-// 						/>
-// 					</div>
-
-// 					<div className="password">
-// 						<input
-// 							type="password"
-// 							placeholder="Password..."
-// 							value={this.state.password}
-// 							onChange={this.update}
-// 							name="password"
-// 						/>
-// 					</div>
-//                     {/* <button class="button" type = "submit" onClick={this.login}>Login</button> */}
-                    
-// 					<input type="submit" value="Login"onClick={this.login} />
-// 				</form>
-
-// 				{/* <Link to="/register">Create an account</Link> */}
-// 			</div>
-// 		);
-// 	}
-// }
-
-// export default Login;
