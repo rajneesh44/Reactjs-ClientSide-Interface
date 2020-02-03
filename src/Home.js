79c53928f1d6b47a57ac856b4abe971e22acb42f
rajneesh44 <rajneeshsharma338@gmail.com>
2020-02-04 01:02:18 +0530

    Feat: Camera added

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import fire from './config/fire';
+import Webcam from "react-webcam";
+ 
+const WebcamComponent = () => <Webcam />;
 
 class Home extends Component {
     constructor(props) {
@@ -15,6 +18,14 @@ class Home extends Component {
         return (
             <div >
                 <h1>You are home</h1>
+                <Webcam
+                    audio={false}
+                    height={720}
+                    // ref={webcamRef}
+                    // screenshotFormat="image/jpeg"
+                    // width={1280}
+                    // videoConstraints={videoConstraints}
+      />
                 <button onClick={this.logout}>Logout</button>
             </div>
             );
