787ca89a1d7c9d4bc25d6b5bcc99e70942151ced
rajneesh44 <rajneeshsharma338@gmail.com>
2020-02-06 23:26:41 +0530

    Updates

---
+++ precache-manifest.4762229284285ee8c751f1c79df78e3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8057d6041970354fe48ba4bc4425934f",
+    "url": "rajneesh44.github.io/Reactjs-ClientSide-Interface/index.html"
+  },
+  {
+    "revision": "b62ffd2b966dcc5533fd",
+    "url": "rajneesh44.github.io/Reactjs-ClientSide-Interface/static/css/main.40c99121.chunk.css"
+  },
+  {
+    "revision": "47399b428b03322ce6a0",
+    "url": "rajneesh44.github.io/Reactjs-ClientSide-Interface/static/js/2.667a6975.chunk.js"
+  },
+  {
+    "revision": "874f9bf2d8ee88e2ebe865d78764a3d9",
+    "url": "rajneesh44.github.io/Reactjs-ClientSide-Interface/static/js/2.667a6975.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b62ffd2b966dcc5533fd",
+    "url": "rajneesh44.github.io/Reactjs-ClientSide-Interface/static/js/main.f2179d2a.chunk.js"
+  },
+  {
+    "revision": "42fd63341f5da051e638",
+    "url": "rajneesh44.github.io/Reactjs-ClientSide-Interface/static/js/runtime-main.7bd011ac.js"
+  },
+  {
+    "revision": "e7237bd36d77c24cd44e5d456224b80e",
+    "url": "rajneesh44.github.io/Reactjs-ClientSide-Interface/static/media/zh.e7237bd3.jpg"
+  }
+]);
